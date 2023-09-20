c0f8592bb5e852458ca4fc18221028cdcac6fea2
Thibault Friedrich <thibault.friedrich@gmail.com>
2023-09-20 15:47:37 -0400

    fix: title sponsor popup

--- src/app/[locale]/SponsorButton.tsx
+++ src/app/[locale]/SponsorButton.tsx
@@ -23,42 +23,13 @@ export default function SponsorButton() {
             className="rounded-full border-4 border-white"
           />
         </div>
-        <Popup.Title>Become a sponsor!</Popup.Title>
+        <Popup.Title>{t('help as a sponsor')}</Popup.Title>
         <Popup.Description>
-          <Popup.Title>{t('help as a sponsor')}</Popup.Title>
-          <Popup.Description>
-            <p className="max-w-screen-2xl">{t('sponsor description')}</p>
-            <p className="pt-5">{t('sponsor benefit')}</p>
-            <p className="pt-5">
-              <ContactUs />
-            </p>
-          </Popup.Description>
-
-          {/* <p className="max-w-screen-2xl">
-            Organizing such a great event is not easy. We need your help to make
-            it happen!
-          </p>
-          <p className="pt-2">
-            As a sponsor, you have different ways to help us (financial,
-            in-kind, media, community).
-          </p>
-          <p className="pt-2">
-            In exchange, we will communicate about you at every steps of the
-            event. It is a great opportunity to reach the{' '}
-            <strong>Dev & Design community</strong> if you have new products or
-            services to promote. It is also a great way to highlight your open
-            positions.
-          </p>
+          <p className="max-w-screen-2xl">{t('sponsor description')}</p>
+          <p className="pt-5">{t('sponsor benefit')}</p>
           <p className="pt-5">
-            Contact us at{' '}
-            <a
-              className="font-semibold text-blue-400"
-              href="mailto:thibault.friedrich@gmail.com"
-            >
-              thibault.friedrich@gmail.com
-            </a>
-            .
-          </p> */}
+            <ContactUs />
+          </p>
         </Popup.Description>
       </Popup>
     </>
